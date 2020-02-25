e94e37018c3e94308e97bb3897ce86dd43952fb3
angelopoole <angelo.poole@outlook.com>
2020-02-25 13:08:46 -0500

    what was complete after code chalenge mock

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,26 +2,53 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
-  render() {
-    return (
-      <Card>
-        <div>
-          <div className="image">
-            <img alt="oh no!" />
-          </div>
-          <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
-          </div>
-          <div className="extra content">
-            <span>
-              <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
-            </span>
-          </div>
-        </div>
-      </Card>
-    )
+  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isFlipped: false
+    }
   }
+
+
+  flipHandler(e) {
+
+    // console.log("hello")
+    // console.log(this.state.isFlipped)
+    this.setState({
+      isFlipped: !this.state.isFlipped
+    })
+  }
+render() {
+  // console.log("pokemon Card", this.props)
+  let { pokemon } = this.props
+  let { name, sprites, stats } = pokemon
+  // let hp = this.props.pokemon.stats[0].hp
+// console.log(this.props.pokemon.stats[0].value)
+
+
+  return (
+    <Card>
+      <div onClick={(e) => this.flipHandler()}>
+        <div className="image">
+          {this.state.isFlipped ? <img src={sprites.back} alt="oh no!" /> : <img src={sprites.front} alt="oh no!" />}
+        </div>
+        <div className="content">
+          <div className="header">{name}</div>
+        </div>
+        <div className="extra content">
+          <span>
+            <i className="icon heartbeat red" />
+            POKEMON HP HERE hp
+            </span>
+        </div>
+      </div>
+    </Card>
+  )
 }
+}
+// add hp later 
+
 
 export default PokemonCard
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,60 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      pokemonData: [],
+      searchForm: "",
+      filteredPokes: []
+    }
+  }
+
+pokemonFlipHandler(){
+  console.log('hello')
+}
+
+handleSearch = (e) => {
+  let filteredPokes = this.state.pokemonData
+  filteredPokes = filteredPokes.filter((pokemon) => {
+    let pokemonName = pokemon.name.toLowerCase()
+    console.log(pokemonName)
+    return pokemonName.indexOf(
+      e.target.value.toLowerCase()) !== -1
+  })
+  this.setState({
+    filteredPokes
+  })
+// console.log(e.target.value)
+// this.setState({searchForm: [e.target.value]})
+
+
+}
+
+  
+
+  componentDidMount(){
+    fetch(`http://localhost:3000/pokemon`)
+    .then(res => res.json())
+    .then(pokemons => this.setState({pokemonData: pokemons, filteredPokes: pokemons}))
+  }
+
+
+
+
   render() {
+    console.log(this.state.pokemonData)
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
         <PokemonForm />
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search onChange={this.handleSearch} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemonData={this.state.pokemonData} pokemonFlipHandler={this.pokemonFlipHandler}/>
       </Container>
     )
   }
