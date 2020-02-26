3b08f96882ec489c7fd86f0fe90b26f37d00a364
David Cha <davidcha17@yahoo.com>
2020-02-26 00:15:41 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,46 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    flipped: false
+  }
+
+
+  handleClick = (e) => {
+    this.setState({
+      flipped: !this.state.flipped
+    })
+  }
+
   render() {
+    // console.log(this.props.pokemon)
+
+    let pokemon = this.props.pokemon
+
+    let { name, sprites, stats } = pokemon
+    
+    let { front, back} = sprites
+
+    let foundHpObject = stats.find(statObj => {return statObj.name === "hp"})
+      
+    console.log(foundHpObject.value)
+
+  // console.log(front)
+
     return (
-      <Card>
+      <Card onClick={this.handleClick} >
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img alt="oh no!" src={ this.state.flipped ? back : front } />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+               {foundHpObject.value} hp
             </span>
           </div>
         </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import PokemonCard from './PokemonCard'
 
 const Search = props => {
+
+  const helpWithChange = (e) => {
+    // console.log(e)
+    // console.log(e.target)
+    // console.log(e.target.value)
+    props.handleSearchTerm(e.target.value)
+  }
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" onChange={props.onChange} />
+        <input className="prompt" onChange={helpWithChange} value={props.searchTerm}/>
         <i className="search icon" />
       </div>
     </div>
